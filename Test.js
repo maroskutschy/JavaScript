c7cde55928c6009a62292e41a19998832b9ac30c
Maros Kutschy <maros.kutschy@operasolutions.com>
2018-11-06 13:34:58 +0100

    initial js file

---
+++ Test.js
@@ -0,0 +1,10 @@
+let year5 = 2018;
+let isLeap5;
+//let restAfterDivision5 = year5 % 4;
+//let restAfterDivision5
+if (year5 % 4 == 0 && (year5 % 100 != 0 || year5 % 400 == 0) ) {
+    isLeap5 = true;
+} else {
+    isLeap5 = false;
+}
+console.log(isLeap5);
