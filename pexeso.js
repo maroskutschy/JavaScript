63dc920f42b995927ff46891e8b6fd84acbd8575
Maros Kutschy <maros.kutschy@operasolutions.com>
2018-12-05 07:35:46 +0100

    added new files + adjusted existing file

---
+++ pexeso.js
@@ -0,0 +1,9 @@
+console.log('ready to code');
+
+// Select 1st element
+//  Add Class .show
+// Select 10th element and 11th element, add class .match
+
+let allElements = document.querySelectorAll('.card');
+console.log(allElements);
+allElements[0].classList.add('show');
